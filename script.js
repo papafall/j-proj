d6ae7520fbd55f3ff770f9e8d97e9f32b5b40ee0
Papa Fall <papa.fall1@gmail.com>
2024-01-20 15:49:13 -0800

    Added code for fizzbuzz game

--- script.js
+++ script.js
@@ -205,18 +205,47 @@ currentWeather();
           */
 const spinButton = document.getElementById("spinButton");
 const spinResult = document.getElementById("spinResult");
+const timer = document.getElementById("timer");
+const reload = document.getElementById("reload");
+let tries = 10;
+let minTries = 0;
 
 function fizzGame() {
   const ranNum = Math.floor(Math.random() * 100) + 1;
   if (ranNum % 3 === 0 && ranNum % 5 === 0) {
-    spinResult.textContent = `${ranNum} - FizzBuzz!`;
+    spinButton.disabled = true;
+    reload.style.display = "block";
+    spinResult.textContent = `${ranNum} - FizzBuzz! - 🏆!!!YOU WIN!!!🏆`;
   } else if (ranNum % 3 === 0) {
-    spinResult.textContent = `${ranNum} - Fizz`;
+    spinResult.textContent = `${ranNum} - Fizz - So close! Try again`;
   } else if (ranNum % 5 === 0) {
-    spinResult.textContent = `${ranNum} - Buzz`;
+    spinResult.textContent = `${ranNum} - Buzz - Almost had it! Try again`;
   } else {
     spinResult.textContent = `${ranNum} - Try again`;
   }
+  tries--;
+  if (tries <= minTries) {
+    spinButton.disabled = true;
+    reload.style.display = "block";
+    if (
+      (ranNum % 3 != 0 && ranNum % 5 != 0) ||
+      ranNum % 3 != 0 ||
+      ranNum % 5 != 0
+    ) {
+      spinResult.textContent = `${ranNum} - Game Over - Better luck next time`;
+    }
+  }
+  timer.textContent = tries;
 }
+
+/*
+function clickTries() {
+  tries--;
+  if (tries <= minTries) {
+    spinButton.disabled = true;
+  }
+  timer.textContent = tries;
+}
+*/
 
 spinButton.addEventListener("click", fizzGame);
